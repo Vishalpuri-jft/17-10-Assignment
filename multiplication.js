ea787a5ed621f57609d5a7d933bfc7e0c9982537
Vishalpuri-jft <118184633+Vishalpuri-jft@users.noreply.github.com>
2022-11-17 22:27:14 +0530

    Delete multiplication.js

--- multiplication.js
+++
@@ -1,9 +0,0 @@
-Array.prototype.Multiply = function(){
-    let mul = 1;;
-    for(let i = 0;i< this.length;i++){
-        mul = mul*this[i];
-    }
-    return mul;
-}
-let arr = [2,6]
-console.log(arr.Multiply())
